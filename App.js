c662ebbd25e88694d31a8f07ab9d92b01e20071a
YiroYi <yiroyi@live.com>
2020-12-05 00:23:59 -0600

    Adding onDelete function

--- App.js
+++ App.js
@@ -20,14 +20,22 @@ export default function App() {
       { id: Math.random().toString(), value: goalTitle}])
   };
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  }
+
   return (
     <View style={styles.screen}>
       <GoalInput onAddGoal ={addGoalHandler} />
       <FlatList keyExtractor={(item, index) => item.id}
                 data={courseGoals}
                 renderItem={itemData => (
-                  <GoalItem onDelete={()=>{console.log("Hello")}}
-                            title={itemData.item.value}/>
+                  <GoalItem
+                    id={itemData.item.id}
+                    onDelete={removeGoalHandler}
+                    title={itemData.item.value}/>
       )}/>
     </View>
   );
