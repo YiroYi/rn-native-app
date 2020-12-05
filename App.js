2c730248e61043ba0bf26e40504222043827754c
YiroYi <yiroyi@live.com>
2020-12-04 23:25:10 -0600

    Adding ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
   const[enteredGoal, setEnteredGoal] = useState('');
@@ -25,9 +25,9 @@ export default function App() {
           />
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
-      <View>
+      <ScrollView >
         {courseGoals.map((goal) => <View style={styles.listItem} key={goal}><Text>{goal}</Text></View>)}
-      </View>
+      </ScrollView>
     </View>
   );
 }
